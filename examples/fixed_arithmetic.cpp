98f3da4a01e3a113cb4e584f14ddca310048cdd7
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-01-16 00:42:06 -0500

    feat(example): Add fixed_arithmetic.cpp example
    - Added 'fixed_arithmetic.cpp' example to showcase fixed-point arithmetic operations.
    - Defined two fixed-point numbers and performed addition, subtraction, multiplication, and division.
    - Demonstrated the handling of fixed-point arithmetic operations using the fixed-point library.
    
    This example illustrates basic fixed-point arithmetic operations in the context of the fixed_point.h library.

---
+++ examples/fixed_arithmetic.cpp
@@ -0,0 +1,32 @@
+/*
+    examples/fixed_arithmetic.cpp
+*/
+#include <iostream>
+
+#include "../include/fixed_point.h"
+
+int main() {
+    std::cout.precision(10);
+
+    // Define two fixed-point numbers
+    fixed16_t a = FLOAT_TO_FIXED(2.5);
+    fixed16_t b = FLOAT_TO_FIXED(1.3);
+
+    // Perform arithmetic operations
+    // addition and subtraction operate as expected
+    fixed16_t sum = a + b;
+    fixed16_t difference = a - b;
+    // multiplication and division need to be handled uniquely
+    // the issue seems due to the way shifting is handled
+    fixed16_t product = a * b;  // this (approximately) produces ~2^14
+    fixed16_t quotient = a / b;  // this produces epsilon
+
+    std::cout << "a: " << FIXED_TO_FLOAT(a) << std::endl;
+    std::cout << "b: " << FIXED_TO_FLOAT(b) << std::endl;
+    std::cout << "a + b: " << FIXED_TO_FLOAT(sum) << std::endl;
+    std::cout << "a - b: " << FIXED_TO_FLOAT(difference) << std::endl;
+    std::cout << "a * b: " << FIXED_TO_FLOAT(product) << std::endl;
+    std::cout << "a / b: " << FIXED_TO_FLOAT(quotient) << std::endl;
+
+    return 0;
+}
