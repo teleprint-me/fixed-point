e5cce1cc062ff1efec25498a186b376dcfd21659
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-01-15 22:04:55 -0500

    feat(example): Add float_to_fixed.cpp example
    - Added 'float_to_fixed.cpp' example with floating-point to fixed-point conversion.
    - Calculated epsilon, the smallest representable value in fixed-point format.
    - Demonstrated the conversion of a float to fixed-point and back to float.
    
    This example showcases basic float-to-fixed conversions using the fixed-point library.

--- examples/float_to_fixed.cpp
+++ examples/float_to_fixed.cpp
@@ -1,8 +1,22 @@
 /*
     examples/float_to_fixed.cpp
 */
+#include <iostream>
+
 #include "../include/fixed_point.h"
 
 int main() {
+    std::cout.precision(10);
+
+    fixed16_t epsilon = INT_TO_FIXED(1) >> FIXED_SIZE; // calculate epsilon
+
+    float f     = 5.7;
+    fixed16_t n = FLOAT_TO_FIXED(f);
+
+    std::cout << "epsilon: " << FIXED_TO_FLOAT(epsilon) << std::endl;
+    std::cout << "float: " << f << std::endl;
+    std::cout << "FLOAT_TO_FIXED: " << n << std::endl;
+    std::cout << "FIXED_TO_FLOAT: " << FIXED_TO_FLOAT(n) << std::endl;
+
     return 0;
 }
