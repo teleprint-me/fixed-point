1a2340525f9aa1bcc1854923ae820a638a992a5e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-07-01 01:51:11 -0400

    refactor: Restructure script for improved readability, maintenance, and extensibility

--- view/bits.py
+++ view/bits.py
@@ -9,50 +9,57 @@
 import argparse
 import struct
 
-parser = argparse.ArgumentParser()
 
-# Add options for different input types
-parser.add_argument(
-    "-f",
-    "--float",
-    help="A floating-point number to convert and pack into bits",
-)
-parser.add_argument(
-    "-i",
-    "--int",
-    help="An integer number to convert and pack into bits",
-)
-parser.add_argument(
-    "-x",
-    "--hex",
-    help="A hexadecimal string to unpack from bits and display as an integer or float",
-)
+def get_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f",
+        "--float",
+        help="A floating-point number to convert and pack into bits",
+    )
+    parser.add_argument(
+        "-i",
+        "--int",
+        help="An integer number to convert and pack into bits",
+    )
+    parser.add_argument(
+        "-x",
+        "--hex",
+        help="A hexadecimal string to unpack from bits and display as an integer or float",
+    )
+    # Define possible byte order values
+    VALID_ORDER = ["<", ">", "@", "=", "!"]
+    parser.add_argument(
+        "-o",
+        "--order",
+        choices=VALID_ORDER,
+        default="@",
+        help="The byte order to use. Default is `@` (native).",
+    )
+    return parser.parse_args()
 
-# Define possible byte order values
-VALID_ORDER = ["<", ">", "@", "=", "!"]
-parser.add_argument(
-    "-o",
-    "--order",
-    choices=VALID_ORDER,
-    default="@",
-    help="The byte order to use. Default is `@` (native).",
-)
-args = parser.parse_args()
 
-# Convert inputs based on the given input type
-if args.float is not None:
-    packed = struct.pack(f"{args.order}f", float(args.float))
-elif args.int is not None:
-    packed = struct.pack(f"{args.order}i", int(args.int))
-elif args.hex:
-    value = int(args.hex, 16)
-    if not (-2147483648 <= value <= 2147483647):
-        raise ValueError(f"Out of Range: -2147483648 <= {value} <= 2147483647")
-    print("int:", value)
-    packed = struct.pack(f"{args.order}i", value)
-else:
-    raise ValueError("Invalid parameter given.")
+def main():
+    args = get_arguments()
 
-bits = "".join(f"{byte:08b}" for byte in packed)
-nibs = " ".join([bits[i : i + 4] for i in range(0, len(bits), 4)])
-print("0b", nibs)
+    # Convert inputs based on the given input type
+    if args.float is not None:
+        packed = struct.pack(f"{args.order}f", float(args.float))
+    elif args.int is not None:
+        packed = struct.pack(f"{args.order}i", int(args.int))
+    elif args.hex:
+        value = int(args.hex, 16)
+        if not (-2147483648 <= value <= 2147483647):
+            raise ValueError(f"Out of Range: -2147483648 <= {value} <= 2147483647")
+        print("int:", value)
+        packed = struct.pack(f"{args.order}i", value)
+    else:
+        raise ValueError("Invalid parameter given.")
+
+    bits = "".join(f"{byte:08b}" for byte in packed)
+    nibs = " ".join([bits[i : i + 4] for i in range(0, len(bits), 4)])
+    print("0b", nibs)
+
+
+if __name__ == "__main__":
+    main()
