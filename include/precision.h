749c15f722798ef3a1c553fa729b5e68a22d617c
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-07-30 13:32:53 -0400

    precision(refactor): Add experimental flex structure as a placeholder and add docstrings

--- include/precision.h
+++ include/precision.h
@@ -23,25 +23,55 @@ typedef uint16_t float16_t;
 typedef uint16_t bfloat16_t;
 typedef uint8_t  float8_t;
 
-// Union to map float to internal integer representation
+/**
+ * @brief A union representing a flexible floating-point representation,
+ *        allowing access to both raw bit data and its corresponding value.
+ *
+ * @param bits  Raw 32-bit integer bit representation of the floating-point number.
+ * @param value The actual 32-bit floating-point value represented by this data structure.
+ */
 typedef union {
     float32_t bits;
     float     value;
 } float_data_t;
 
-// Function declarations
+/**
+ * @brief Represents a flexible floating point data structure capable of
+ *        handling different precisions and formats.
+ *
+ * @param value  The actual floating-point value represented by this data structure.
+ * @param type   Indicates the specific precision or format (e.g., float32, bfloat16)
+ */
+typedef struct {
+    float_data_t value;
+    data_type_t  type;
+} float_flex_t;
 
-// Encode standard 32-bit floating-point
+/**
+ * @brief Encodes a given float value into its corresponding 32-bit representation.
+ *
+ * @param[in] value The floating point number to be encoded.
+ *
+ * @return The resulting encoded 32-bit integer representation of the input value.
+ */
 float32_t encode_float32(float value);
-float     decode_float32(float32_t flex);
+
+/**
+ * @brief Decodes a given 32-bit integer representation into its corresponding float value.
+ *
+ * @param[in] bits The encoded 32-bit integer bit representation of the floating-point number.
+ *
+ * @return The decoded actual 32-bit floating-point value represented by this data structure.
+ */
+float decode_float32(float32_t bits);
 
 // Encode standard 16-bit floating-point
 float16_t encode_float16(float value);
-float     decode_float16(float16_t flex);
+float     decode_float16(float16_t bits);
 
 // Encode extended 8-bit float
 float8_t encode_float8(float value);
-float    decode_float8(float8_t flex);
+float    decode_float8(float8_t bits);
 
 // Encode Google Brain 16-bit float
 bfloat16_t encode_bfloat16(float value);
