12f7b0a7d4290d5a20415247e8910012e8557dff
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-07-31 03:22:03 -0400

    examples(simple): Refactor to simplify implementation and automate sampling

--- examples/simple_ieee_754.cpp
+++ examples/simple_ieee_754.cpp
@@ -34,40 +34,36 @@ float decode_float32(uint32_t bits) {
 }
 
 // Function to print the binary representation of a 32-bit number
-void print_binary(void* bits, size_t width) {
-    uint32_t* repr = (uint32_t*) bits;
-
-    for (int step = width - 1; step >= 0; step--) {
-        printf("%d", ((*repr) >> step) & 1);
-
-        if (0 == step % 4) {
+void print_binary(uint32_t bits, size_t width) {
+    for (int i = width - 1; i >= 0; i--) {
+        printf("%d", (bits >> i) & 1);
+        if (0 == i % 4 && 0 != i) {
             printf(" ");
         }
     }
-
     printf("\n");
 }
 
-void print_bit_representation(uint32_t bits, size_t s, size_t e, size_t m) {
-    // Print the floating-point value
+void print_bit_representation(uint32_t bits) {
     printf("%f -> ", decode_float32(bits));
 
-    // Extract the sign bit
-    uint32_t sign = (bits >> (s - 1)) & 0x1;
+    // Extract the sign bit (1 bit)
+    uint32_t sign = (bits >> 31) & 0x1;
     printf("%d ", sign);
 
     // Extract the exponent bits (8 bits)
-    uint32_t exponent = (bits >> m) & 0xFF;
-    for (int i = (e - 1); i >= 0; i--) {
+    uint32_t exponent = (bits >> 23) & 0xFF;
+    for (int i = 7; i >= 0; i--) {
         printf("%d", (exponent >> i) & 0x1);
-        if (i % 4 == 0) {
+        if (i == 4) {
             printf(" ");
         }
     }
+    printf(" ");
 
     // Extract the mantissa bits (23 bits)
     uint32_t mantissa = bits & 0x7FFFFF;
-    for (int i = m - 1; i >= 0; i--) {
+    for (int i = 22; i >= 0; i--) {
         printf("%d", (mantissa >> i) & 0x1);
         if (i % 4 == 0) {
             printf(" ");
@@ -76,17 +72,13 @@ void print_bit_representation(uint32_t bits, size_t s, size_t e, size_t m) {
     printf("\n");
 }
 
-int main() {
-    // float value = PI;
-    const size_t bit_width = 32; // float is 32-bits wide
-    float        value     = 2;
-
+void print_metadata(float value, const size_t bit_width) {
     // Encode the float into IEEE-754 binary32 representation
     uint32_t encoded = encode_float32(value);
     printf("value (float): %f\n", value);
     printf("encoded (hex): 0x%08X\n", encoded);
     printf("encoded (binary): ");
-    print_binary(&encoded, bit_width);
+    print_binary(encoded, bit_width);
 
     // Decode the IEEE-754 binary32 representation back into a float
     float decoded = decode_float32(encoded);
@@ -94,7 +86,30 @@ int main() {
 
     // literal internal representation
     printf("literal representation: ");
-    print_bit_representation(encoded, 32, 8, 23);
+    print_bit_representation(encoded);
+}
+
+int main() {
+    float        value     = 1.0f;
+    const size_t bit_width = 32;
+
+    // Sample a small range of numbers
+    for (size_t step = 1; step < 5; step++) {
+        print_metadata(value, bit_width);
+        value += (float) step;
+        puts("---");
+    }
+
+    // negative number
+    print_metadata(-1.0f, bit_width);
+    puts("---");
+
+    // irrational number
+    print_metadata(PI, bit_width);
+    puts("---");
+
+    // repeating fraction
+    print_metadata(REPEAT, bit_width);
 
     return 0;
 }
